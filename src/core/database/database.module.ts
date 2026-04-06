988cef97361f611d6a5e8ab73b4644b6252afc52
Shafi <shafi.fleekbd@gmail.com>
2026-04-06 10:58:47 +0600

    final set up

---
+++ src/core/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        dialect: 'postgres',
+        host: config.get('DB_HOST'),
+        port: +config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        autoLoadModels: false,
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
